35e3ca391ec8cd03eee607f77a0818b35f68882c
iceprincefounder <iceprincefounder@qq.com>
2017-05-19 11:31:58 +0800

    a lot of code in alTriplanar_plus and cryptomatte is missing,because of .git folder exsit

--- LightChaserAnim/cryptomatte/alShaders_build_maya/alLayer/CMakeFiles/alLayerColorUI.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/alLayerColorUI"
-  "alLayerColor.mtd"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/alLayerColorUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
